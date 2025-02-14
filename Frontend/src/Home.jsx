7dd5b5abb6a2530747e8535184d9625c3e3dc69f
Aadi-kommula <aadikommula@gmail.com>
2025-02-14 13:10:10 +0530

    added backend logic

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,53 +1,152 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-const Home = () => {
-  const [tab, setTab]=useState(1)
-  const [task, setTask]=useState(1)
- 
-  const handleTab =(tab)=>{
-    setTab(tab)
-    console.log(tab)
-  }
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-  const handleAdd = ()=>{    
-    axios.post('http://localhost:8080/new-task',{task})
-  }
+const Home = () => {
+  const [tab, setTab] = useState(1);
+  const [task, setTask] = useState('');
+  const [category, setCategory] = useState('');
+  const [todos, setTodos] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
+
+  const [activeTodos, setActiveTodos] = useState(null);
+  const [completedTodos, setCompletedTodos] = useState(null);
+
+  const handleCategory = (category) => {
+    setCategory(category);
+    console.log(category)
+  };
+
+  const handleTabs = (tab) => {
+    setTab(tab);
+  };
+
+  const handleAddTask = (e) => {
+    if(task.length>1){
+    e.preventDefault();
+    axios.post('http://localhost:5000/new-task', { task, category })
+      .then(res => {
+        setTodos(res.data);
+        setTask('');
+      });
+    }
+  };
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/read-tasks')
+      .then(res => {
+        setTodos(res.data);
+      });
+  }, []);
+
+  const [updateId, setUpdateId] = useState(null);
+  const [updatedTask, setUpdatedTask] = useState('');
+  const handleEdit = (id, task) => {
+    setIsEdit(true);
+    setTask(task);
+    setUpdatedTask(task);
+    setUpdateId(id);
+  };
+
+  const updateTask = () => {
+    axios.post('http://localhost:5000/update-task', { updateId, task })
+      .then(res => {
+        setTodos(res.data);
+        setTask('');
+        setIsEdit(false);
+      });
+  };
+
+  const handleDelete = (id) => {
+    axios.post('http://localhost:5000/delete-task', { id })
+      .then(res => {
+        setTodos(res.data);
+      });
+  };
+
+  const handleComplete = (id) => {
+    axios.post('http://localhost:5000/complete-task', { id })
+      .then(res => {
+        setTodos(res.data);
+      });
+  };
+
   return (
     <div className='bg-gray-100 w-screen h-screen'>
-      <div className='flex flex-col items-center justify-center w-screen h-90'>
-        <h2 className='text-2xl font-bold mb-5 text-pink-600' >ToDo List</h2>
-        <div className='gap-4 flex'>
-        <input 
-        className='p-2 border outline-none w-62 border-blue-200 rounded-md cursor-text bg-white' 
-        type="text" 
-        placeholder="Enter task"
-        onChange={(e)=>{
-          setTask(e.target.value)
-        }} />
-        <button
-        className='bg-blue-500 text-white px-4 rounded-md cursor-pointer'
-        onClick={() => handleAdd()}>Add</button>
+      <div className='flex flex-col w-screen h-screen justify-center items-center'>
+        <div>
+          <h2 className='font-bold text-2xl mb-4'>ToDo List</h2>
         </div>
-        <div className='flex justify-between w-60 mt-5'>
-            <p className={ `${tab==1 ? 'text-blue-700 cursor-pointer hover:text-green-600 ' : 'text-black cursor-pointer '} `} onClick={()=> handleTab(1)}>All</p>
-            <p className={ `${tab==2 ? 'text-blue-700 cursor-pointer hover:text-green-600 ' : 'text-black cursor-pointer '} `} onClick={()=> handleTab(2)}>Active</p>
-            <p  className={ `${tab==3 ? 'text-blue-700 cursor-pointer hover:text-green-600 ' : 'text-black cursor-pointer '} `} onClick={()=> handleTab(3)}>Completed</p>
-          </div>
-          <div className='flex w-84 gap-26 p-4 mt-6 bg-white rounded-md'>
-           <div>
-           <p className='text-md font-bold'>Task 1</p>
-            <p>12-02-2025</p>
-            <p>Status:Active</p>
-           </div>
-           <div>
-            <p className='text-cyan-600 cursor-pointer'>Edit ✍️</p>
-            <p className='text-rose-600 cursor-pointer'>Delete ⛔</p>
-            <p className='text-green-600 cursor-pointer'>Completed ✅</p>
-           </div>
-          </div>
-         </div>
-    </div>
-  )
-}
+        <div className='flex gap-3'>
+          <input value={task} onChange={e => setTask(e.target.value)} type='text' placeholder='Enter todo' className='w-64 p-2 outline-none border border-blue-300 rounded-md' />
+          < >
+            {isEdit ? <button className='bg-blue-600 text-white px-4 rounded-md' onClick={updateTask}>Update</button> : <button className='bg-blue-600 text-white px-4 rounded-md' onClick={handleAddTask}>add</button>}
+          </>
+        </div>
 
-export default Home
+        <div className='flex text-sm w-80 justify-evenly mt-5 mb-2'>
+          <div> <input onClick={() => handleCategory('work')} type="radio" name="category" id="work" /> Work</div>
+          <div> <input onClick={() => handleCategory('personal')} type="radio" name="category" id="Personal" /> Personal</div>
+          <div> <input onClick={() => handleCategory('urgent')} type="radio" name="category" id="Urgent" /> Urgent</div>
+        </div>
+
+        <div className='flex text-sm w-80 justify-evenly mt-4 mb-2'>
+          <p onClick={() => handleTabs(1)} className={`${tab === 1 ? 'text-blue-700' : 'text-black'} cursor-pointer`}>All</p>
+          <p onClick={() => handleTabs(2)} className={`${tab === 2 ? 'text-blue-700' : 'text-black'} cursor-pointer`}>Active</p>
+          <p onClick={() => handleTabs(3)} className={`${tab === 3 ? 'text-blue-700' : 'text-black'} cursor-pointer`}>Completed</p>
+        </div>
+
+
+        {/* All tasks Tab */}
+
+        {tab == 1 && todos?.map(todo => (
+          <div className='flex justify-between bg-white p-3 w-80 mt-5 rounded-md'>
+            <div>
+              <p className='text-lg font-semibold'>{todo?.task}</p>
+              <p className='text-xs text-gray-600'>{new Date(todo.createdAt).toLocaleDateString()}</p>
+              <p className='text-sm text-gray-700'>Status : {todo.status}</p>
+              <p className='text-sm text-gray-700'>Category: {todo.category}</p>
+            </div>
+
+            <div className='flex flex-col text-sm justify-center items-center'>
+              {todo.status == 'completed' ? null : <button className='text-blue-600 cursor-pointer' onClick={() => handleEdit(todo.id, todo.task)}>Edit</button>}
+              <button className='text-red-500 cursor-pointer' onClick={() => handleDelete(todo.id)}>Delete</button>
+              {todo.status == 'completed' ? null : <button className='text-green-600 cursor-pointer' onClick={() => handleComplete(todo.id)}>Completed</button>}
+            </div>
+          </div>
+        ))}
+
+        {tab == 2 && todos?.filter(todo => todo.status == 'active').map(todo => (
+          <div className='flex justify-between bg-white p-3 w-80 mt-5 rounded-md'>
+            <div>
+              <p className='text-lg font-semibold'>{todo?.task}</p>
+              <p className='text-xs text-gray-600'>{new Date(todo.createdAt).toLocaleDateString()}</p>
+              <p className='text-sm text-gray-700'>Status : {todo.status}</p>
+            </div>
+
+            <div className='flex flex-col text-sm justify-start items-start'>
+              {todo.status == 'completed' ? null : <button className='text-blue-600 cursor-pointer' onClick={() => handleEdit(todo.id, todo.task)}>Edit</button>}
+              <button className='text-red-500 cursor-pointer' onClick={() => handleDelete(todo.id)}>Delete</button>
+              {todo.status == 'completed' ? null : <button className='text-green-600 cursor-pointer' onClick={() => handleComplete(todo.id)}>Completed</button>}
+            </div>
+          </div>
+        ))}
+
+        {tab == 3 && todos?.filter(todo => todo.status == 'completed').map(todo => (
+          <div className='flex justify-between bg-white p-3 w-80 mt-5 rounded-md'>
+            <div>
+              <p className='text-lg font-semibold'>{todo?.task}</p>
+              <p className='text-xs text-gray-600'>{new Date(todo.createdAt).toLocaleDateString()}</p>
+              <p className='text-sm text-gray-700'>Status : {todo.status}</p>
+            </div>
+
+            <div className='flex flex-col text-sm justify-center items-center'>
+              <button className='text-red-500 cursor-pointer' onClick={() => handleDelete(todo.id)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
